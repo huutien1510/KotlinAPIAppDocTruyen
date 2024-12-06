b7c72ad3bf7ba6019a7377d927fef23d61c3a5a9
T-Huy <lehuy099@gmail.com>
2024-12-06 21:54:32 +0700

    update lich su doc truyen

--- src/main/java/com/example/apidoctruyen/Controller/tusachController.java
+++ src/main/java/com/example/apidoctruyen/Controller/tusachController.java
@@ -1,13 +1,21 @@
 package com.example.apidoctruyen.Controller;
 
+import com.example.apidoctruyen.entity.Chapter;
+import com.example.apidoctruyen.entity.Lichsudoctruyen;
+import com.example.apidoctruyen.entity.Taikhoan;
+import com.example.apidoctruyen.entity.Truyen;
+import com.example.apidoctruyen.model.LichSuDocTruyenDto;
 import com.example.apidoctruyen.model.LichsudoctruyenModel;
+import com.example.apidoctruyen.repository.ChapterRepository;
+import com.example.apidoctruyen.repository.TaiKhoanRepository;
 import com.example.apidoctruyen.repository.lichsudoctruyenRepository;
+import com.example.apidoctruyen.service.LichSuDocTruyenService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+
 @RestController
 @RequestMapping("/tusach")
 public class tusachController {
@@ -15,10 +23,21 @@ public class tusachController {
     @Autowired
     lichsudoctruyenRepository repo;
 
+    @Autowired
+    LichSuDocTruyenService lichSuDocTruyenService;
+
 
     @GetMapping("/lichsu/{idtaikhoan}")
     public List<LichsudoctruyenModel> getListTruyenDaDoc(@PathVariable int idtaikhoan) {
         return repo.getListTruyenDaDoc(idtaikhoan);
     }
+
+    @PostMapping("/lichsu")
+    public void saveLichSuDoc(@RequestBody LichSuDocTruyenDto lichSuDocTruyenDto) {
+        Lichsudoctruyen result = lichSuDocTruyenService.saveOrUpdateLichSuDoc(lichSuDocTruyenDto);
+    }
+
+
+
 
 }
